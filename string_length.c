09faf8a97598fcf7b287ad5af17475757d1fb7c7
Łukasz Sroczyński <LukSroczynski@users.noreply.github.com>
2017-06-09 09:19:42 +0200

    Create string_length.c

--- string_length.c
+++ string_length.c
@@ -4,12 +4,19 @@
 
 int main(void) 
 {
-    //
+    // get a string from user
     string s = get_string();
+    
+    // set char-counter to 0
     int n = 0;
+    
+    // increment n till end of the string. 
+    // \0 - sign stored in memory indicates end of the string
     while(s[n] != '\0') 
     {
         n++;
     }
+    
+    // print string length
     printf("%i\n", n);
 }
